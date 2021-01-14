4c207aa5191d4389ac576a2e9ac4c621adddae08
naum <naum43312016@gmail.com>
2021-01-14 13:54:55 +0200

    created auth controller

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ const app = express();
 require("dotenv").config();
 const cors = require('cors')
 const fileUpload = require('express-fileupload');
-
+const AuthController = require('./controllers/AuthController');
 //User file upload to upload images
 app.use(fileUpload({
     createParentPath: true
 }));
 app.use(cors())
 app.use(express.json());
+
+app.post('/signup',AuthController.signUp);
+
+app.get('/test',AuthController.test);
 
 module.exports = app;
