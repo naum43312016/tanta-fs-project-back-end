1c8c8c762cc6277e6bcabffaa51646f4d1ea52b9
naum <naum43312016@gmail.com>
2021-01-15 00:09:02 +0200

    changed server.js

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const app = require('./app');
 const port = process.env.PORT || 3001;
-
 const dbConnection = require('./config/dbConnection')
-dbConnection.connectToDbServer()
 
-app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`);
-})
+async function connectToDatabaseAndStartServer(){
+    try{
+        await dbConnection.connectToDbServer();
+        app.listen(port, () => {
+            console.log(`App listening at http://localhost:${port}`);
+        })
+    }catch(e){
+        console.log("Can't connect to database: " + e);
+    }
+}
+
+connectToDatabaseAndStartServer();
